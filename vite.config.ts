7a0c95547147bcafde2bdd6f0bf13a3c5611ec30
Monaal5 <monaalmamen@gmail.com>
2025-06-25 11:01:57 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,23 @@ export default defineConfig(({ mode }) => ({
     include: ['react', 'react-dom', 'pdfjs-dist/build/pdf'],
   },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
     commonjsOptions: {
       include: [/pdfjs-dist/, /node_modules/],
     },
